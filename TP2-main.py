153c86137d371b0bbce0bd3b4118129c77f4785f
Martin-elmqirmi <martin.elmqirmi@gmail.com>
2022-03-14 17:41:31 -0400

    Programme pour la Tâche 1 + ajout des images + ajout du fichier csv

--- TP2-main.py
+++ TP2-main.py
@@ -1,5 +1,22 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def get_plotbox(name, data):
+    sns.boxplot(data=data, orient='h')
+    print(max(data))
+    plt.savefig('' + name + '.png')
+
+
 def main():
-    print("Hello World!")
+    df = pd.read_csv('jfreechart-stats.csv')
+    print(df.head())
+    print(df.keys())
+    get_plotbox('WMC', df['WMC'])
+
 
 if __name__ == "__main__":
     main()
